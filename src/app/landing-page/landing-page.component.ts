a117cb1507f5e370597379a715a9f4c5052dfe6b
Hanan-Hussein <hanhuss035@gmail.com>
2022-04-18 13:04:36 +0300

    ft:hide accesskey-hidden access key

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GitInfoService } from '../git-info.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing-page.component.css']
 })
 export class LandingPageComponent implements OnInit {
-
-  constructor() { }
+  inputData: string='Hanan-Hussein';
+  constructor(private gitinfo: GitInfoService,public router:Router) { }
 
   ngOnInit(): void {
   }
-
+  userSearch() {
+    this.gitinfo.userApi(`https://api.github.com/users/${this.inputData}`)
+    this.router.navigate(['user'])
+  }
+  repoSearch() {
+    // this.gitinfo.repoApi(`https://api.github.com/search/repositories?q=${this.inputData}&order=desc&sort=stars&page=1&per_page=10`)
+    this.router.navigate(['repositories'])
+    this.gitinfo.userRepoSearch(`https://api.github.com/search/repositories?q=${this.inputData}&order=desc&sort=stars&page=1&per_page=17`)
+  }
 }
